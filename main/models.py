34205f5c24e111d56adec2bae44bc07464d65bcc
Mereke-bz <mereke.bazarbaev842@gmail.com>
2025-08-16 09:46:01 +0500

    cделали views, urls настроили, шаблогны катологи

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -13,6 +14,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
+    
     
 
 class Product(models.Model):
@@ -33,3 +38,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+
+  
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
